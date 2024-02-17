04075324dbb3a7a4a43dfd4efeb22816e3252830
Ashish Kulkarni <ashishkulkarni19999@gmail.com>
2024-02-17 21:07:37 +0530

    counter added;

---
+++ src/kulp-counter.ts
@@ -0,0 +1,25 @@
+import { LitElement, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("kulp-counter")
+export class KulpCounter extends LitElement {
+    @property({ type: Number }) count = 0;
+
+    increment() {
+        this.count++;
+    }
+
+    decrement() {
+        this.count--;
+    }
+
+    render() {
+        return html`
+        <div>
+            <h1>Count: ${this.count}</h1>
+            <button @click=${this.increment}>Increment</button>
+            <button @click=${this.decrement}>Decrement</button>
+        </div>
+        `;
+    }
+}
